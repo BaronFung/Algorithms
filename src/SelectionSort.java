d2ee78f8019ed86bfbcc994b25f8f49e9e68963c
shaonan <156690750@qq.com>
2020-05-09 23:06:32 +0800

    添加选择排序算法

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,26 @@
 public class SelectionSort {
+    public static void sort(int[] a){
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            int min = i;
+            for (int j = i; j < len; j++) {
+                if (a[j]<a[min])
+                    min = j;
+            }
+            swap(a,i,min);
+        }
+    }
+    private static void swap( int[] a, int i, int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,23,67,4,22,0,5,16,8,66};
+        sort(arr);
+        for (int item : arr) {
+            System.out.print(item+" ");
+        }
+    }
 }
